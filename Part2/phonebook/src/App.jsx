e04f250c54a6308d5b242e3791c03b93ba6d22c5
Marcus Bru <marcusbrudev@gmail.com>
2024-12-12 16:25:02 -0500

    Solucion ejericio 2.11

--- Part2/phonebook/src/App.jsx
+++ Part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,18 +8,33 @@ import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
+
+/*{ name: 'Arto Hellas', number: '040-123456', id: 1 },
+{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }*/
+
 const App = ()=>{
 
-  const[persons,setPersons]= useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const[persons,setPersons]= useState([])
 
   const[newName,setNewName]= useState('')
   const[newPhoneNumber, setNewPhoneNumber]= useState("")
   const[nameToSearch, setNameToSearch] = useState("")
+
+  const hook = ()=>{
+      axios
+      .get('http://localhost:3001/persons')
+      .then(response=>{
+        setPersons(response.data)
+      })
+ 
+  }
+
+  useEffect(hook,[]);
+
+
+
 
   let names= persons.map((element)=> element.name)
   
