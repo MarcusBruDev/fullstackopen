9723d80631bfb34b70a14d646157a388adf223a6
Marcus Bru <marcusbrudev@gmail.com>
2025-01-20 20:59:06 -0500

    Se modifica la url del fronten

--- Part2/phonebook/src/services/persons.js
+++ Part2/phonebook/src/services/persons.js
@@ -1,23 +1,25 @@
 import axios from "axios";
 
+const baseUrl = '/api/persons'
+
 const getAll= ()=>{
-    const request = axios.get('http://localhost:3001/api/persons')
+    const request = axios.get(baseUrl)
     return request.then(response=> response.data)
 }
 
 const create=(newObject)=>{
     
-    const request = axios.post('http://localhost:3001/api/persons',newObject)
+    const request = axios.post(baseUrl,newObject)
     return request.then(response=>response.data)
 }
 
 const deleted= (id)=>{
-    const request= axios.delete(`http://localhost:3001/api/persons/${id}`)
+    const request= axios.delete(`${baseUrl}/${id}`)
     return request.then(response=>response.data)
 }
 
 const update = (id,newObject)=>{
-    const request = axios.put(`http://localhost:3001/api/persons/${id}`,newObject)
+    const request = axios.put(`${baseUrl}/${id}`,newObject)
     return request.then(response=> response.data)
 }
 
