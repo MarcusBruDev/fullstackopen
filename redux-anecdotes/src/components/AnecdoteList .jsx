5b13651fa5f547920980c05ebc2fd5aa3a16853b
Marcus Bru <marcusbrudev@gmail.com>
2025-06-25 22:37:14 -0500

    Comentario

--- redux-anecdotes/src/components/AnecdoteList .jsx
+++ redux-anecdotes/src/components/AnecdoteList .jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux"
 import { vote } from "../reducers/anecdoteReducer";
 
-///hello
+///hello part6
 const Anecdote =({anecdote,handleClick})=>{
     return(
         <>
