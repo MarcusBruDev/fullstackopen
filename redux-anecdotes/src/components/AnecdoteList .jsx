aacafbfecf7eed3ac7764bd0eb17209f6babce1d
Marcus Bru <marcusbrudev@gmail.com>
2025-06-25 22:34:35 -0500

    Ejercicios 6.3 to 6.8 part6

--- redux-anecdotes/src/components/AnecdoteList .jsx
+++ redux-anecdotes/src/components/AnecdoteList .jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux"
 import { vote } from "../reducers/anecdoteReducer";
 
-
+///hello
 const Anecdote =({anecdote,handleClick})=>{
     return(
         <>
